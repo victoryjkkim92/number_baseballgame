9bd71fa1f2d73b9729ed1bd181487bbedba7287c
jukyung kim <victoryjkkim92@gmail.com>
2018-10-26 10:11:30 +0900

    JS : src/index.js modified

--- src/index.js
+++ src/index.js
@@ -1,69 +1,39 @@
 // 시도 버튼에 클릭 이벤트가 일어났을 때
 const tryButton = document.querySelector(".input-box-try");
-const inputBoxes = document.querySelector(".input-boxes");
 const inputBoxDigit1 = document.querySelector(".input-box-digit-1");
 const inputBoxDigit2 = document.querySelector(".input-box-digit-2");
 const inputBoxDigit3 = document.querySelector(".input-box-digit-3");
-let roundNum = 1;
-let tryNum = []; // 시도 숫자 테스트 케이스
-const answer = ["1", "2", "3"]; // 실제 정답
-const tryNumSize = answer.length;
-let result = "";
 
-// 시도 버튼 눌렀을 때
 tryButton.addEventListener("click", e => {
-  tryNum = [];
-  const round = document.createElement("span");
+  // div 생성 후 'turn-list'에 붙여준다
   const turnListEl = document.querySelector(".turn-list");
-  // cloneNode에서 해당 node의 children 까지 복제하려면 true, 해당 node 만 복제하려면 false
-  const turnListItemEl = inputBoxes.cloneNode(true);
-
-
-  tryNum.push(inputBoxDigit1.value);
-  tryNum.push(inputBoxDigit2.value);
-  tryNum.push(inputBoxDigit3.value);
-  oneGame();
-
-  const resultField = document.createElement("span");
-  resultField.textContent = result;
-
-  // 리스트에 복제되서 추가되는 input box들 수정 막는 코드
-  for (let i = 0; i < turnListItemEl.children.length; i++) {
-    turnListItemEl.children[i].setAttribute("readonly", "");
-  }
-
-  round.textContent = roundNum++ + "회";
-  turnListItemEl.prepend(round);
-  turnListItemEl.appendChild(resultField)
+  const turnListItemEl = document.createElement("div");
+  turnListItemEl.classList.add("list-items");
   turnListEl.appendChild(turnListItemEl);
+  const round = document.createElement("span");
+  round.textContent = "1회";
+  turnListItemEl.appendChild(round);
 
+  // input박스 복사해서 추가
+  let inputBoxEl = inputBoxDigit1.cloneNode()
+  turnListItemEl.appendChild(inputBoxEl);
+  inputBoxEl = inputBoxDigit2.cloneNode()
+  turnListItemEl.appendChild(inputBoxEl);
+  inputBoxEl = inputBoxDigit3.cloneNode()
+  turnListItemEl.appendChild(inputBoxEl);
 
-
-  inputBoxDigit1.value = "";
-  inputBoxDigit2.value = "";
-  inputBoxDigit3.value = "";
-  inputBoxDigit1.focus();
-  console.log('result' + result);
-
+  // console.log(inputBoxDigit1.value)
   e.preventDefault();
 });
 
-// 자동 포커스 이동
-inputBoxes.childNodes.forEach(el => {
-  el.addEventListener("input", e => {
-    if (el.nextElementSibling) {
-      el.nextElementSibling.focus();
-    } else {
-      el.parentElement.nextElementSibling.focus();
-    }
-  });
-});
+// 한번 더 버튼에 클릭 이벤트가 일어났을 때
+// const resetButton = document.querySelector(".input-box-reset");
 
+// resetButton.addEventListener("click", e => {});
 
-
-/*
-야구 게임 로직
-*/
+let tryNum = [4, 3, 1]; // 시도 숫자 테스트 케이스
+const answer = [1, 2, 3]; // 실제 정답
+const tryNumSize = tryNum.length;
 
 // 시도 횟수 한번 판단 함수
 function oneGame() {
@@ -72,26 +42,20 @@ function oneGame() {
   if (strike === 3) {
     // 스트라이크가 3이면 즈어어엉답!!
     console.log("즈어어엉답!!");
-    result = "즈어엉답!";
   } else {
     // 정답이 아니면 strike 와 ball 횟수 출력
     console.log(strike + " strike");
     console.log(ball + " ball");
-    result = ball + 'B ' + strike + 'S'; // 1B 0S
   }
   if (strike === 0 && ball === 0) {
     // strike 와 ball 이 모두 0이라면 아우우웃ㅅ@!@
     console.log("아우우웃~!@");
-    result = "OUT";
   }
 }
 
 // 스트라이크 몇개인지 판별 함수
 function howManyStrike() {
   let strike = 0;
-
-  console.log(tryNum);
-  console.log(answer);
   for (let i = 0; i < tryNumSize; i++) {
     if (tryNum[i] === answer[i]) {
       // 첫번째 자리, 두번째 자리, 세번째 자리 차례차례 비교해서 같을때마다 strike 1씩 증가.
@@ -117,16 +81,4 @@ function howManyBall() {
   return ball;
 }
 
-function randomAnswer() {
-  const a = Math.floor(Math.random() * 9);
-  const b = Math.floor(Math.random() * 9);
-  const c = Math.floor(Math.random() * 9);
-  return a.toString() + b.toString() + c.toString();
-}
-
-console.log (randomAnswer())
-
 oneGame();
-
-
-
